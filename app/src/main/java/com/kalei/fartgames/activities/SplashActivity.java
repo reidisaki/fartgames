c9a9b861be4048b6fe0e941de1376c2c4499daf3
Reid Isaki <reidisaki@yahoo.com>
2016-02-15 19:00:01 -0800

    cleaned up unused code

--- app/src/main/java/com/kalei/fartgames/activities/SplashActivity.java
+++ app/src/main/java/com/kalei/fartgames/activities/SplashActivity.java
@@ -5,22 +5,14 @@ import com.kalei.fartgames.R;
 import com.kalei.fartgames.fragments.SplashFragment;
 import com.kalei.fartgames.utils.IntentGenerator;
 
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.os.Handler;
-import android.transition.Explode;
-import android.view.Window;
 
 public class SplashActivity extends FartActivity {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
-        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
-        // set an exit transition
-        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-            getWindow().setExitTransition(new Explode());
-        }
+
         super.onCreate(savedInstanceState);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.splash_container, SplashFragment.newInstance()).commit();
@@ -31,11 +23,5 @@ public class SplashActivity extends FartActivity {
                 overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
             }
         }, 2000);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
     }
 }
